bf9300590b300db737ece9306b1ad651808d326b
prarthanaj <prarthanaj.xworkz@gmail.com>
2021-10-02 23:53:08 +0530

    initial commit

---
+++ AnimalsRunner.java
@@ -0,0 +1,32 @@
+class AnimalsRunner{
+      public static void main(String[] value)
+	  {
+	    String dog="Dogs";
+        String eleph="Elephants";
+        String cow="Cow";
+        String buffalo="Buffalo";
+        String pig="Pig";
+        String koala="Koala";
+		String sloth="Sloth";
+		String pbear="Polarbear";
+		String panda="Panda";
+		String kangaroo="Kangaroo";
+        String[] animals={dog,eleph,cow,buffalo,pig,koala,sloth,pbear,panda,kangaroo};
+        int size=animals.length;
+//no. of elements
+        System.out.println("list of favourite animals:"+size);
+//index is to access elements 
+        System.out.println("Index 0:"+animals[0]);
+//first element starts at 0
+         System.out.println("Index 1:"+animals[1]);
+         System.out.println("Index 2:"+animals[2]);
+         System.out.println("Index 3:"+animals[3]);
+         System.out.println("Index 4:"+animals[4]);
+         System.out.println("Index 5:"+animals[5]);
+		 System.out.println("Index 6:"+animals[6]);
+         System.out.println("Index 7:"+animals[7]);
+         System.out.println("Index 8:"+animals[8]);
+//animals[8]--prints eigth element in the array list
+
+      }
+}
