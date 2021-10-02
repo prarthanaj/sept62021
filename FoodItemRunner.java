33bea400870fefb621cc0b443d5fb04ba9294ec1
prarthanaj <prarthanaj.xworkz@gmail.com>
2021-10-03 00:34:47 +0530

    fooditemrunner

---
+++ FoodItemRunner.java
@@ -0,0 +1,8 @@
+class FoodItemRunner{
+	public static void main(String[] values)
+	{
+	String[] item={"vada","dosa","idly","roti"};
+		String address=FoodItem.foodItems(item);
+		System.out.println(address);
+	}
+}
